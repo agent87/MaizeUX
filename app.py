3f6d5591aa5899b3428508cc44d7cf311e95aea8
agent87 <arnauldkayonga1@gmail.com>
2023-01-06 13:50:30 +0200

    Added new file & requirements.txt

--- app.py
+++ app.py
@@ -3,12 +3,20 @@ import streamlit as st
 
 
 
-def home():
-    st.title("Ahabanza")
+
 
 def demo():
     st.title("Gerageza Ikoranabuhanga ryacu!")
-    img = st.file_uploader("Upload Image", type=['png', 'jpg', 'jpeg'])
+    upload_col, capture_col = st.columns(2)
+    
+    img = st.file_uploader("Uploading ifoto y' igihingwa cyawe aha!", type=['png', 'jpg', 'jpeg'])
+    if img is not None:
+        st.image(img, caption="Ifoto y' igihingwa cyawe", use_column_width=True)
+
+    img = st.camera_input("Capture ifoto y' igihingwa cyawe aha!")
+    if img is not None:
+        st.image(img, caption="Ifoto y' igihingwa cyawe", use_column_width=True)
+
 
 def contact():
     st.sidebar.title("Contact")
@@ -17,19 +25,27 @@ def contact():
 
 
 #the navbar function
-def navbar():
-    st.sidebar.title("Menu")
-    global mode
-    home_button = st.sidebar.button("Ahabanza", on_click=home)
-    demo_button = st.sidebar.button("Gerageza Ikoranabuhanga ryacu!", on_click=demo)
-
-    #contact banner
-    contact()
+def contact_navbar():
+    st.sidebar.title("Contact")
+    st.sidebar.info("Email: mugenzik@gmail.com")
+    st.sidebar.info("Phone: +250780587387")
+    st.sidebar.map()
 
 
 #the main function
 def main():
-    navbar() #call the navbar function
+    #the navbar
+    contact_navbar()
+
+    #home section
+    st.title("Ahabanza")
+    st.write("Reba ubuzima bw' ibihingwa byawe!")
+    st.image('assets/maize.jpg')
+
+    #the demo section
+    demo()
+    
+    
     
 
 
