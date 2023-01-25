51db202d26695f3bd30d90674d5e99dccee6582f
agent87 <arnauldkayonga1@gmail.com>
2023-01-25 20:40:49 +0200

    Fixed cv2 bug in prediction function

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st 
-import cv2
 import numpy as np
-import keras
+from tensorflow import keras
+
+#Hide Tf Error
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 
@@ -10,25 +13,59 @@ class demo:
         st.title("Gerageza Ikoranabuhanga ryacu!")
         self.model = keras.models.load_model(model_path)
     
+        self.demo_input_ui()
+
+    def demo_input_ui(self):
         img = st.file_uploader("Uploading ifoto y' igihingwa cyawe aha!", type=['png', 'jpg', 'jpeg'])
         if img is not None:
-            img_ = self.process_img(img)
-            self.predict_img(img_)
+            img_id = self.save_img(img)
+            img = self.process_img(img_id)
+            prediction = self.predict_img(img)
 
         img = st.camera_input("Capture ifoto y' igihingwa cyawe aha!")
         if img is not None:
-            img_ = self.process_img(img)
-            self.predict_img(img_)
+            img_id = self.save_img(img)
+            img = self.process_img(img_id)
+            prediction = self.predict_img(img)
 
-    def process_img(self, img):
-        file_bytes = np.asarray(bytearray(img.read()), dtype=np.uint8) / 255
-        opencv_image = cv2.imdecode(file_bytes, 1)
-        resized_img = cv2.resize(opencv_image, (224, 224))
-        st.image(opencv_image, channels="BGR", caption="Ifoto y' igihingwa cyawe", use_column_width=True)
-        return resized_img
+
+    def save_img(self, image_file):
+        id =  len(os.listdir("temp/"))
+        with open(f"temp/img-{id}.jpg", "wb") as f:
+            f.write(image_file.getbuffer())
+        return id
+
+    def process_img(self, img_id):
+        #Load flower jpeg image from local and set target size to 224 x 224
+        img = keras.utils.load_img(f'temp/img-{img_id}.jpg', target_size=(224,224))
+
+        #display image using streamlit
+        st.image(img, caption='Igihingwa cyawe', use_column_width=True)
+
+        #convert image to array
+        input_img = keras.preprocessing.image.img_to_array(img)
+        input_img = np.expand_dims(input_img, axis=0)
+
+        return input_img
+
 
     def predict_img(self, img):
-        print(self.model.predict(img))
+        pred = self.model.predict(img)
+        print(pred)
+        print(np.argmax(pred))
+        pred = np.argmax(pred, axis=1)
+        if pred == 0:
+            print("Class 1")
+
+        elif pred == 1:
+            print("Class 2")
+
+        elif pred == 2:
+            print("Class 3")
+
+        elif pred == 3:
+            print("Class 4")
+        return pred
         
 
 def contact():
